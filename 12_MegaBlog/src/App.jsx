0231656f261bac4e2bea9e1f1eb9c8cdcb87f7e4
Simple-Calculator <dibyanandmishra@gmail.com>
2026-02-05 20:36:31 +0530

    feat: integrate Tailwind CSS and set up Redux for authentication
    - Created a Redux slice for authentication with login and logout actions.
    - Implemented user authentication state management in App component using Redux.
    - Added Header and Footer components for layout structure.
    
    - Set up Redux store in main.jsx and wrapped App component with Provider.

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -1,12 +1,42 @@
+import { useState, useEffect } from 'react'
 import './App.css'
+import {useDispatch} from "react-redux"
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice' 
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}));
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  if (!loading) {
+    return (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header/>
+            <main>
+              {/* <Outlet/> */}
+            </main>
+          <Footer/>
+        </div>
+      </div>
+    )
+  }
+  else{
+    <div>Loading...</div>
+  }
 }
 
 export default App
