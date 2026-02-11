b0c66c73f8918b631392b0514b4138c7a2aaabff
Simple-Calculator <dibyanandmishra@gmail.com>
2026-02-11 21:53:57 +0530

    Login, Signup, PostCard, Select, AuthLayout components

--- 12_MegaBlog/src/components/Header/Header.jsx
+++ 12_MegaBlog/src/components/Header/Header.jsx
@@ -18,22 +18,22 @@ function Header() {
       name: "Login",
       slug: "/login",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "Signup",
       slug: "/signup",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "All Posts",
       slug: "/all-posts",
       active: authStatus,
-  },
-  {
+    },
+    {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
-  },
+    },
   ]
 
 
